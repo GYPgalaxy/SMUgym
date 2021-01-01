efa0c31b17ad08a1d222aa0aaa7a5deda2bd7b26
Galaxy <galaxygyp@163.com>
2021-01-02 01:03:56 +0800

    -前端-文件中的内容已修改为贴切课设的设计

--- EX/gym/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-# localhost:8000/gym/.......
-def index(request):
-    return HttpResponse("Hello, world. You're at the gym index.")
